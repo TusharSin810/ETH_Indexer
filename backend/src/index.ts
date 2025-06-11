2050a3dbfd0175e870d1fead2eddb1b5177227d2
TusharSin810 <tsinghal666@gmail.com>
2025-06-12 00:27:37 +0530

    indexer init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import pg from "pg";
-import { HDNodeWallet, Wallet } from "ethers6";
+import { HDNodeWallet} from "ethers6";
 import { mnemonicToSeedSync } from "bip39";
-import { MNUENOMICS } from "../config";
+import { MNUENOMICS } from "./config";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 const seed = mnemonicToSeedSync(MNUENOMICS);
 
+app.use(express.json());
+
+app.use(cors());
 
 app.post("/signUp", (req,res) => {
     const username = req.body.username;
@@ -16,7 +20,8 @@ app.post("/signUp", (req,res) => {
     const userId = 1;
     const hdNode = HDNodeWallet.fromSeed(seed);
     const child = hdNode.derivePath(`m/44'/60'/${userId}'/0`);
-    console.log(child);
+    console.log(child.privateKey);
+    console.log(child.address);
 
     res.json({
         userId
